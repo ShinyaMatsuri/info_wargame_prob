034a871d2c9f7aebe6b7ee794dc68a3e879d19bb
ShinyaMatsuri <min83250909@gmail.com>
2021-10-21 16:47:04 -0700

    ROP backup

--- pwnable/basic_ROP/main.c
+++ pwnable/basic_ROP/main.c
@@ -9,7 +9,16 @@ void func(void)
 	read(0, name, 256);
 }
 
+int binit()
+{
+	setvbuf(stdout, 0, 2, 0);
+	setvbuf(stdin, 0, 2, 0);
+	return setvbuf(stderr, 0, 2, 0);
+}
+
 int main(void)
 {
+	binit();
+	puts("welcome to ROP world");
 	func();
 }
